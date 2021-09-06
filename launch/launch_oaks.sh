b9fac928d60c72d82acfea279a0de789156af07a
hwgat <hwgatford@gmail.com>
2021-09-06 11:01:44 +1000

    Created new oak detector launch file that will launch all 3 oaks at once

--- launch/launch_oaks.sh
+++ launch/launch_oaks.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-# source virtual environment running python3 with depthai installed
-. ../dai_bridge_ws/venv/bin/activate
+echo "Make sure you have sourced your virtual environment running python3 with depthai installed"
 
-echo Enter oak camera id number
+echo "Enter oak camera id number"
 read oak_id
 echo Reading detections from oak $oak_id
 echo Detections occur in oak_${oak_id}_frame 
 echo Publishing detections to /spencer/perception_internal/detected_persons/oak_$oak_id
 
-echo Visualise images? (Y/n)
-read visualise
+echo Visualize images? (Y/n)
+read visualize
 
-if [ "$visualise" = "Y" ]
+if [ "$visualize" = "Y" ]
 then
-python3 src/development/scripts/publish_yolo_detections_single_camera.py --oak_camera_num $oak_id --ros_tf_frame oak_${oak_id}_frame --ros_output_topic /spencer/perception_internal/detected_persons/oak_$oak_id --visualise True
+python3 src/development/scripts/publish_yolo_detections_single_camera.py --oak_camera_num $oak_id --ros_tf_frame oak_${oak_id}_frame --ros_output_topic /spencer/perception_internal/detected_persons/oak_$oak_id --visualize True
 else
 python3 src/development/scripts/publish_yolo_detections_single_camera.py --oak_camera_num $oak_id --ros_tf_frame oak_${oak_id}_frame --ros_output_topic /spencer/perception_internal/detected_persons/oak_$oak_id
 fi
---
+++ launch/launch_oaks_combined.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "Make sure you have sourced your virtual environment running python3 with depthai installed"
+
+echo "Visualize images? (Y/n)"
+read visualize
+
+for oak_id in 0 1 2
+do 
+    echo Reading detections from oak $oak_id
+    echo Detections occur in oak_${oak_id}_frame 
+    echo Publishing detections to /spencer/perception_internal/detected_persons/oak_$oak_id
+
+
+    if [ "$visualize" = "Y" ]
+    then
+    python3 src/development/scripts/publish_yolo_detections_single_camera.py --oak_camera_num $oak_id --ros_tf_frame oak_${oak_id}_frame --ros_output_topic /spencer/perception_internal/detected_persons/oak_$oak_id --visualize True
+    else
+    python3 src/development/scripts/publish_yolo_detections_single_camera.py --oak_camera_num $oak_id --ros_tf_frame oak_${oak_id}_frame --ros_output_topic /spencer/perception_internal/detected_persons/oak_$oak_id
+    fi
+done
+
