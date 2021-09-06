0407746ea1dcfba584f5969fcf7374694c7e0bb1
hrigroup <hrigroup@CPR-J100-0368.CPR-J100-0368>
2021-09-06 00:12:04 -0400

    Altered so OAK camera detectors launch in background. To kill use ps command in terminal then kill the process by number

--- launch/launch_oaks_combined.sh
+++ launch/launch_oaks_combined.sh
@@ -14,9 +14,9 @@ do
 
     if [ "$visualize" = "Y" ]
     then
-    python3 src/development/scripts/publish_yolo_detections_single_camera.py --oak_camera_num $oak_id --ros_tf_frame oak_${oak_id}_frame --ros_output_topic /spencer/perception_internal/detected_persons/oak_$oak_id --visualize True
+    python3 src/development/scripts/publish_yolo_detections_single_camera.py --oak_camera_num $oak_id --ros_tf_frame oak_${oak_id}_frame --ros_output_topic /spencer/perception_internal/detected_persons/oak_$oak_id --visualize True &
     else
-    python3 src/development/scripts/publish_yolo_detections_single_camera.py --oak_camera_num $oak_id --ros_tf_frame oak_${oak_id}_frame --ros_output_topic /spencer/perception_internal/detected_persons/oak_$oak_id
+    python3 src/development/scripts/publish_yolo_detections_single_camera.py --oak_camera_num $oak_id --ros_tf_frame oak_${oak_id}_frame --ros_output_topic /spencer/perception_internal/detected_persons/oak_$oak_id &
     fi
 done
 
