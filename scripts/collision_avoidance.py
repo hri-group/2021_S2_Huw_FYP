1368e11226266bf8b61cea852f68fd18dad9e27d
hrigroup <hrigroup@CPR-J100-0368.CPR-J100-0368>
2021-11-22 23:16:27 -0500

    Wesley's collision avoidance scripts

---
+++ scripts/collision_avoidance.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import LaserScan
+import math
+
+def callbackScan(data):
+	global isObstacleFront, isObstacleBack
+	rospy.loginfo('================= in callbackScan =============')
+
+	pointCountFront = 0
+	pointCountBack = 0
+	# define rectangular region in front and back of robot to check for obstacle
+	# x points left of robot, y points back of robot
+	y_min_front = -1.5
+	y_max_front = 0 # how close an obstacle in front has to be before the robot stops when going forward
+	x_min_front = -0.25
+	x_max_front = 0.25
+
+	y_min_back = 0 # how close an obstacle behind has to be before the robot stops going backwards
+	y_max_back = 2.0
+	x_min_back = -0.25
+	x_max_back = 0.25
+
+	pointsThres = 2 # min number of pointcloud points to be considered as obstacle and not noise
+
+	theta = data.angle_min
+
+	rospy.loginfo(data.ranges[0])
+
+	for pointDist in data.ranges:
+		# zero degree points left of robot 
+		x_coord = pointDist * math.cos(theta)
+		y_coord = pointDist * math.sin(theta)
+		if (x_coord > x_min_front) and (x_coord < x_max_front) and (y_coord > y_min_front) and (y_coord < y_max_front):
+			pointCountFront=pointCountFront + 1 
+		elif (x_coord > x_min_back) and (x_coord < x_max_back) and (y_coord > y_min_back) and (y_coord < y_max_back):
+			pointCountBack=pointCountBack+1
+		theta = theta + data.angle_increment
+
+	rospy.loginfo('pointCountFront %f', pointCountFront)
+	rospy.loginfo('pointCountBack %f', pointCountBack)
+
+	if pointCountFront > pointsThres:
+		isObstacleFront = True 
+		rospy.loginfo('isObstacleFront true')
+	else:
+		isObstacleFront = False
+		rospy.loginfo('isObstacleFront false')
+
+	if pointCountBack > pointsThres:
+		isObstacleBack = True
+		rospy.loginfo('isObstacleBack true')
+	else:
+		isObstacleBack = False
+		rospy.loginfo('isObstacleBack false')
+
+
+
+if __name__ == '__main__':
+	rospy.init_node('collision_avoidance',anonymous=True)
+
+	rospy.Subscriber("/scan", LaserScan, callbackScan)
+
+	rate = rospy.Rate(100) #100Hz
+	while not rospy.is_shutdown():
+		rate.sleep()
+	rospy.loginfo('node shutdown')
